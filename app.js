fdb6c0c722abea4b19a566e8a4ff064217128198
Mehcoder42 <Mehcoder42@gmail.com>
2020-10-27 20:49:55 +0700

    adding clear grid button

--- app.js
+++ app.js
@@ -1,37 +1,44 @@
 let gridSize = 16;
 function makeGrid() {
+    let width = 600;
+    let height = 600;
 
     for (let i = 0; i < gridSize * gridSize; i++) {
         let makeDiv = document.createElement('div');
         makeDiv.className = 'grid-item';
 
         document.getElementById('grid-container').appendChild(makeDiv);
-        document.getElementById('grid-container').style.gridTemplateColumns = "repeat(" + gridSize + ", 30px";
-        document.getElementById('grid-container').style.gridTemplateRows = "repeat(" + gridSize + ", 30px";
+        document.getElementById('grid-container').style.gridTemplateColumns = "repeat(" + gridSize + ", " + width / gridSize + "px)";
+        document.getElementById('grid-container').style.gridTemplateRows = "repeat(" + gridSize + ", " + height / gridSize + "px)";
+
     }
 }
 
 
 function removeGrid() {
+    gridItem = document.querySelectorAll('.grid-item');
     gridItem.forEach(function (e) {
         e.remove();
     })
 }
 makeGrid();
 
-
-const gridItem = document.querySelectorAll('.grid-item');
+let total = 0;
+let gridItem;
 function hover() {
+    gridItem = document.querySelectorAll('.grid-item');
     gridItem.forEach(function (e) {
+        let gridColor = 255;
         e.addEventListener('mouseenter', function () {
-            e.classList.toggle('black');
+            gridColor -= 25.5;
+            e.style.backgroundColor = `rgb(${gridColor}, ${gridColor}, ${gridColor})`;
         })
     })
 }
 
 hover();
 
-const customGridButton = document.querySelector('#customGridButton')
+const customGridButton = document.querySelector('#customGridButton');
 
 customGridButton.addEventListener('click', function () {
     gridSize = prompt('Enter a Custom Grid');
@@ -40,3 +47,14 @@ customGridButton.addEventListener('click', function () {
     hover();
 })
 
+function clearGrid () {
+    gridItem = document.querySelectorAll('.grid-item');
+    gridItem.forEach(function (e) {
+        e.style.backgroundColor = 'rgb(255,255,255)';
+        gridColor = 255;
+    })
+}
+
+const clearGridButton = document.querySelector('#clearGridButton');
+
+clearGridButton.addEventListener('click', clearGrid);
